04818f234d6bd3ea817cce1633ae034bdb91d979
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-10-21 17:09:44 +0300

    Add tasks for week 4.

---
+++ Week_04 - Lambda Expressions/task1.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+; Define a higher order procedure that:
+; - is called `my-identity` and simulates the `identity` procedure;
+; - accepts a procedure and returns a lambda that accepts an argument and applies that procedure to it;
+; - accepts a predicate and returns a lambda that accepts an argument and applies the negated predicate to it;
+; - accepts two procedures and returns their composition over an argument of a lambda procedure;
+; - returns a procedure that is the partial application of f over x.
+
+
+
+; write 2 tests for subtask 1
