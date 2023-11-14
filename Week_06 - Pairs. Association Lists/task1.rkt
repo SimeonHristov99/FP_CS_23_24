858ce1f35be8b95c1cd48463e2c29c2cb69ae35e
SimeonHristov99 <s.e.hristov99@gmail.com>
2023-11-14 21:33:23 +0200

    Adding back week 6.
    Somehow it got deleted.

---
+++ Week_06 - Pairs. Association Lists/task1.rkt
@@ -0,0 +1,43 @@
+#lang racket
+
+; Define a procedure that returns
+; the value of a given key from an associative list.
+
+(define (assoc-rec key xs)
+  (cond
+    [(null? xs) #f]
+    [(equal? key (caar xs)) (cdar xs)]
+    [else (assoc-rec key (cdr xs))]
+    )
+  )
+
+(define (assoc-hop key xs)
+  (let
+      ([filter-res (filter (λ (x) (equal? key (car x))) xs)])
+    (if (null? filter-res)
+        #f
+        (cdar filter-res)
+        )
+      )
+  )
+
+(define (assoc-builtin key xs)
+  (let
+      ([assoc-res (assoc key xs)])
+    (if assoc-res
+        (cdr assoc-res)
+        #f
+        )
+      )
+  )
+
+; using a recursive process
+(equal? (assoc-rec 1 (list '(2 . "Two") '(3 . "Three") '(1 . "One"))) "One")
+
+; using a higher order procedure
+(equal? (assoc-hop 1 (list '(2 . "Two") '(3 . "Three") '(1 . "One"))) "One")
+(equal? (assoc-hop 42 (list '(2 . "Two") '(3 . "Three") '(1 . "One"))) #f)
+
+; using a built-in procedure
+(equal? (assoc-builtin 1 (list '(2 . "Two") '(3 . "Three") '(1 . "One"))) "One")
+(equal? (assoc-builtin 42 (list '(2 . "Two") '(3 . "Three") '(1 . "One"))) #f)
