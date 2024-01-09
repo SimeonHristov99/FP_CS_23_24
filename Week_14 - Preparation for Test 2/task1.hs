b2f64f13f54fe9725e56989c17ed307dc903fdb4
SimeonHristov99 <s.e.hristov99@gmail.com>
2024-01-09 19:08:21 +0200

    Add solutions.

--- Week_14 - Preparation for Test 2/task1.hs
+++ Week_14 - Preparation for Test 2/task1.hs
@@ -4,8 +4,17 @@ main = do
     print $ maximumLevel t2 == 3
     print $ maximumLevel t3 == 3
 
+data BTree a = Nil | Node a (BTree a) (BTree a)
+ deriving (Show)
+
 t1 = Node 94 (Node 73 Nil (Node 80 Nil Nil)) (Node 87 (Node 73 Nil Nil) Nil)
 t2 = Node 94 (Node 73 Nil (Node 80 Nil Nil)) (Node 87 (Node 73 Nil Nil) (Node 7 Nil Nil))
 t3 = Node 94 (Node 0 Nil (Node 42 Nil Nil)) (Node 0 (Node 42 Nil Nil) (Node 42 Nil Nil))
 
+maximumLevel :: (Num a, Ord a) => BTree a -> Int
+maximumLevel t = snd $ foldr1 (\ child@(valsC, _) parent@(valsP, _) -> if sum valsC > sum valsP then child else parent) $ takeWhile ((not . null) . fst) [ (getLevel t k, k + 1) | k <- [0 .. ]]
 
+getLevel :: BTree a -> Int -> [a]
+getLevel Nil _ = []
+getLevel (Node value _ _) 0 = [value]
+getLevel (Node _ left right) k = getLevel left (k - 1) ++ getLevel right (k - 1)
--- Week_14 - Preparation for Test 2/task2.hs
+++ Week_14 - Preparation for Test 2/task2.hs
@@ -2,6 +2,17 @@ main :: IO()
 main = do
     print $ isPerfectlyBalanced t1 == True
 
+data BTree a = Nil | Node a (BTree a) (BTree a)
+
 t1 = Node 'A' (Node 'B' (Node 'D' Nil Nil) (Node 'D' Nil Nil)) (Node 'C' (Node 'D' Nil Nil) (Node 'E' Nil Nil))
 
+size :: BTree a -> Int
+size Nil = 0
+size (Node _ left right) = 1 + size left + size right
 
+height :: BTree a -> Int
+height Nil = 0
+height (Node _ left right) = 1 + max (height left) (height right)
+
+isPerfectlyBalanced :: BTree a -> Bool
+isPerfectlyBalanced t = size t == 2^(height t) - 1
